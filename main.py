80d6fa88c6309b0cdd5c42fcd0fde14929c1b92f
Asma Ben merar <asmabenmerar09.com>
2024-12-18 13:04:14 +0100

    Affichage  de la grille initiale

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ frame = np.array([
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0]
 ])
+def afficher_grille(grille):
+    for index_ligne in range(len(grille)):
+        ligne = grille[index_ligne]
+        for index_cellule in range(len(ligne)):
+            cellule = ligne[index_cellule]
+            print(cellule, end='')
+        print() #saut de ligne apres chaque ligne
+    print() #saut de ligne apres toute la grille
+afficher_grille(frame)
