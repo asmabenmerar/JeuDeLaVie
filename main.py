f9f2f7488a9f0f583690b7bccee0c3993184d94a
Asma Ben merar <asmabenmerar09.com>
2024-12-18 13:18:47 +0100

    Ajout de la fonction de calcul des voisins vivants

--- main.py
+++ main.py
@@ -18,3 +18,12 @@ def afficher_grille(grille):
         print() #saut de ligne apres chaque ligne
     print() #saut de ligne apres toute la grille
 afficher_grille(frame)
+
+def compute_number_neighbors(padded_frame, index_line, index_colomn):
+    #calculer le nombre de voisins vivants
+    number_neighbors = 0
+    for ligne in range(index_line - 1, index_line +2):
+        for colonne in range(index_colomn - 1, index_colomn + 2):
+            if (ligne != index_line or colonne != index_colomn):
+                number_neighbors += padded_frame[ligne, colonne]
+        return number_neighbors        
